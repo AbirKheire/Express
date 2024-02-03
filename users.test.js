925fb7b94ae90ec4a958eb7ace2887ded2f98ec9
Abir <139335805+AbirKheire@users.noreply.github.com>
2024-02-03 16:32:15 +0100

    Add files via upload

---
+++ users.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+
+const app = require("../src/app");
+
+describe("GET /api/users", () => {
+  it("should return all users", async () => {
+    const response = await request(app).get("/api/users");
+
+    expect(response.headers["content-type"]).toEqual(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/users/:id", () => {
+  it("should return one user", async () => {
+    const response = await request(app).get("/api/users/1");
+
+    expect(response.headers["content-type"]).toEqual(/json/);
+    expect(response.status).toEqual(200);
+  });
+
+  it("should return no user", async () => {
+    const response = await request(app).get("/api/users/0");
+    
+    expect(response.headers["content-type"]).toEqual(/json/);
+    expect(response.status).toEqual(404);
+  });
+});
