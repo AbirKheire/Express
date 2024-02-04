8fe8c6ee4ce89fc45867339a3d1b9d2e3b16ae64
Abir K <kheireddineabir@gmail.com>
2024-02-04 13:13:40 +0100

    quest 02 done

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
-
 const app = require("../src/app");
+
 
 describe("GET /api/users", () => {
   it("should return all users", async () => {
     const response = await request(app).get("/api/users");
 
-    expect(response.headers["content-type"]).toEqual(/json/);
+    expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
 });
@@ -15,14 +15,13 @@ describe("GET /api/users/:id", () => {
   it("should return one user", async () => {
     const response = await request(app).get("/api/users/1");
 
-    expect(response.headers["content-type"]).toEqual(/json/);
+    expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
 
   it("should return no user", async () => {
     const response = await request(app).get("/api/users/0");
     
-    expect(response.headers["content-type"]).toEqual(/json/);
     expect(response.status).toEqual(404);
   });
 });
